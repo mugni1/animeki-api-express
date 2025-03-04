f9d7b09563ce6e7a115122fdb85116a9caf5499d
mugni1 <asepabdulmugni22@gmail.com>
2025-03-04 14:46:54 +0700

    add

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/", async (req, res) => {
         link: "https://animeki-api-express.vercel.app/search/?s=naruto",
       },
     },
-    scrape_with: "AAM",
+    SCRAPER: "INITIAL A",
   });
 });
 app.get("/jadwal", async (req, res) => {
